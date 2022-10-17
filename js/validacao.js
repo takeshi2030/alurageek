1f04d53d68988fc9542151387f6e7a35688e626c
André Maemura <andremaemura@gmail.com>
2022-10-17 14:13:47 -0300

    cadastro produto

--- js/validacao.js
+++ js/validacao.js
@@ -43,12 +43,36 @@ const mensagemDeErro = {
     cpf: {
         valueMissing:'O campo CPF não pode estar vazio.',
         customError: 'O CPF digitado não é válido.'
-    }
+    },
+    cep: {
+        valueMissing:'O campo CEP não pode estar vazio.',
+        patternMismatch: 'O CPF digitado não é válido.',
+        customError: 'Não foi possível encontrar o CEP.'
+    },
+    logradouro: {
+        valueMissing:'O campo logradouro não pode estar vazio.',
+    },
+    cidade: {
+        valueMissing:'O campo cidade não pode estar vazio.'
+    },
+    estado: {
+        valueMissing:'O campo estado não pode estar vazio.'
+    },
+    url: {
+        valueMissing:'O campo url não pode estar vazio.'
+    },
+    categoria: {
+        valueMissing:'O campo categoria não pode estar vazio.'
+    },
+    preco: {
+        valueMissing:'O campo preco não pode estar vazio.'
+    }   
 }
 
 const validadores = {
     dataNascimento:input => validaDataNascimento(input),
-    cpf:input => validaCPF(input)
+    cpf:input => validaCPF(input),
+    cep:input => recuperarCEP(input)
 }
 
 function mostraMensagemDeErro(tipoDeInput, input) {
@@ -60,12 +84,6 @@ function mostraMensagemDeErro(tipoDeInput, input) {
     })
     return mensagem
 }
-
-const dataNascimento = document.querySelector('#nascimento')
-
-dataNascimento.addEventListener('blur', (event) => {
-    validaDataNascimento(event.target)
-})
 
 function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value)
@@ -89,7 +107,7 @@ function validaCPF(input) {
     const cpfFormatado = input.value.replace(/\D/g, '')
     let mensagem = ''
     
-    if (!checaCPFRepetido(cpfFormatado)) {
+    if (!checaCPFRepetido(cpfFormatado) || !chegaEstruturaCPF(cpfFormatado)) {
         mensagem = 'O CPF digitado não é válido.'
     }
 
@@ -118,4 +136,79 @@ function checaCPFRepetido(cpf) {
     })
 
     return cpfValido
+}
+
+
+// VERIFICADOR DE DIGITO CPF - ESTUDAR MELHOR!
+
+function chegaEstruturaCPF(cpf) {
+    const multiplicador = 10
+
+    return checaDigitoVerificador(cpf, multiplicador)
+}
+
+function checaDigitoVerificador(cpf, multiplicador){
+    if (multiplicador >= 12) {
+        return true
+    }
+
+    let multiplicadorInicial = multiplicador
+    let soma = 0
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('')
+    const digitoVerificador = cpf.charAt(multiplicador - 1)
+    for(let contador = 0; multiplicadorInicial > 1 ; multiplicadorInicial--){
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
+        contador++
+    }
+
+    if (digitoVerificador == confirmaDigito(soma)) {
+        return checaDigitoVerificador(cpf, multiplicador + 1)
+    }
+
+    return false
+
+}
+
+function confirmaDigito(soma) {
+    return 11 - (soma % 11)
+}
+
+//CEP
+
+function recuperarCEP(input) {
+    const cep = input.value.replace(/\D/g, '')
+    const url = `https://viacep.com.br/ws/${cep}/json`
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'content-type': 'application/json;charset=utf-8'
+        }
+    }
+
+    if (!input.validity.patternMismatch && !input.validity.valueMissing) {
+        fetch(url,options).then(
+            response => response.json()
+        ).then(
+            data => {
+                if(data.erro) {
+                    input.setCustomValidity('Não foi possível buscar o CEP')
+                    return
+                }
+                input.setCustomValidity('')
+                preencheCampoComCPF(data)
+                return
+            }
+        )
+    }
+}
+
+function preencheCampoComCPF(data) {
+    const logradouro = document.querySelector('[data-tipo="logradouro"]')
+    const cidade = document.querySelector('[data-tipo="cidade"]')
+    const estado = document.querySelector('[data-tipo="estado"]')
+
+    logradouro.value = data.logradouro
+    cidade.value = data.localidade
+    estado.value = data.uf
 }
