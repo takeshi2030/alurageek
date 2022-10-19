75768a64645f09caceba1891e896618ea012ad2c
André Maemura <andremaemura@gmail.com>
2022-10-18 23:12:08 -0300

    inicio cadastro produto

--- js/adiciona-produto.js
+++ js/adiciona-produto.js
@@ -2,6 +2,18 @@ const formProduto = document.getElementById('add-produto')
 
 formProduto.addEventListener('submit', (evento) => {
     evento.preventDefault()
+
+    criaElemento(evento.target.elements['link-img'].value,
+    evento.target.elements['categoria'].value,
+    evento.target.elements['nome'].value,
+    evento.target.elements['preco'].value,
+    evento.target.elements['descricao'].value,)
 })
 
-console.log(formProduto)
+function criaElemento(img, categoria, nome, preco, descricao) {
+    console.log(img)
+    console.log(categoria)
+    console.log(nome)
+    console.log(preco)
+    console.log(descricao)
+}
