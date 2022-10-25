e9d4cd05967b8a65c954ae1f81f0678be2d7a733
André Maemura <andremaemura@gmail.com>
2022-10-25 19:48:57 -0300

    Update produtos.js

--- js/produtos.js
+++ js/produtos.js
@@ -1,16 +1,6 @@
-// Buscar a uls no html
-const sectionStarWars = document.getElementById('lista-star-wars')
-const sectionConsoles = document.getElementById('lista-consoles')
-const sectionDiversos = document.getElementById('lista-diversos')
+
 const todosProdutos = document.getElementById('lista-produtos')
 
-// FUNCTION SEARCH 
-const content = document.querySelector('.content')
-const search = document.querySelector('#searchInput')
-
-search.addEventListener('input', (event) => {
-    console.log(event.target.value)
-})
 
 
 // TRABALHANDO 'API'
@@ -20,47 +10,6 @@ const listaProdutos = () => {
     return fetch(apiProdutos)
     .then(dados => dados.json())
 }
-
-listaProdutos()
-.then(resposta => {
-    const todasCategorias = resposta
-
-    // Filtrar no JSON por SECTION
-    const produtosStarWars = todasCategorias.filter(categoria => categoria.section==="star_wars-section")
-    const produtosConsole = todasCategorias.filter(categoria => categoria.section==="consoles-section")
-    const produtosDiversos = todasCategorias.filter(categoria => categoria.section==="diversos-section")
-    
-    // Imprimir as informações no html
-    produtosStarWars.forEach(produto => sectionStarWars.innerHTML += 
-        (`<li class="mb-4 mx-2 lg:mx-1.5">
-            <img class="w-full" src="${produto.image}" alt="foto-produto">
-            <div class="flex flex-col justify-items-start mt-2">
-            <span class="text-base">${produto.nome}</span>
-            <span class="text-base font-bold">${produto.preco}</span>
-            <a href=""><button class="text-sm font-bold text-left text-blue-500">Ver produto</button></a>
-            </div>
-        </li>`))
-
-    produtosConsole.forEach(produto => sectionConsoles.innerHTML += 
-        (`<li class="mb-4 mx-2 lg:mx-1.5">
-            <img class="w-full" src="${produto.image}" alt="foto-produto">
-            <div class="flex flex-col justify-items-start mt-2">
-            <span class="text-base">${produto.nome}</span>
-            <span class="text-base font-bold">${produto.preco}</span>
-            <a href=""><button class="text-sm font-bold text-left text-blue-500">Ver produto</button></a>
-            </div>
-        </li>`))
-
-    produtosDiversos.forEach(produto => sectionDiversos.innerHTML += 
-        (`<li class="mb-4 mx-2 lg:mx-1.5">
-            <img class="w-full" src="${produto.image}" alt="foto-produto">
-            <div class="flex flex-col justify-items-start mt-2">
-            <span class="text-base">${produto.nome}</span>
-            <span class="text-base font-bold">${produto.preco}</span>
-            <a href=""><button class="text-sm font-bold text-left text-blue-500">Ver produto</button></a>
-            </div>
-        </li>`))
-})
 
 fetch(apiProdutos)
 .then(dados => dados.json())
