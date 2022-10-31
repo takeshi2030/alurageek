9d399296bbd8b0be9272269d22d5ead11ad729d1
André Maemura <andremaemura@gmail.com>
2022-10-31 18:11:49 -0300

    testes lista produtos

--- js/produtosHome.js
+++ js/produtosHome.js
@@ -1,7 +1,30 @@
 // Buscar a uls no html
-const sectionStarWars = document.getElementById('lista-star-wars')
+/* const sectionStarWars = document.getElementById('lista-star-wars')
 const sectionConsoles = document.getElementById('lista-consoles')
-const sectionDiversos = document.getElementById('lista-diversos')
+const sectionDiversos = document.getElementById('lista-diversos') */
+const listasections = document.getElementById('lista-sections')
+
+const listSection = [
+    'star-wars',
+    'consoles',
+    'diversos' 
+]
+
+listSection.forEach((section, idx, arr) => {
+    listasections.innerHTML += `
+    <li>
+        <section id="section-${section}" class="mb-4">
+            <div class="flex items-center justify-between px-4 md:px-12 lg:px-40 mb-4">
+                <h3 class="text-xl lg:text-4xl font-bold text-gray-600">${section}</h3>
+                <button class="flex items-center text-blue-500 hover:text-blue-700"><p class="text-sm md:text-lg font-bold mr-2 md:mr-4">Ver tudo</p><i class="fa-solid fa-arrow-right"></i></button>
+            </div>
+            <!------GRID PRODUTOS------->
+            <ul id="lista-${section}" class="px-2 md:px-10 lg:px-40 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 mb-2">
+                <!-- PRODUTOS -->
+            </ul>
+        </section>
+    </li>`
+    })
 
 // TRABALHANDO 'API'
 const apiProdutos = 'js/db.json'
@@ -21,15 +44,21 @@ listaProdutos()
     const produtosDiversos = todasCategorias.filter(categoria => categoria.section==="diversos-section")
     
     // Imprimir as informações no html
-    produtosStarWars.forEach(produto => sectionStarWars.innerHTML += 
-        (`<li class="mb-4 mx-2 lg:mx-1.5">
+    produtosStarWars.forEach((produto, idx, arr) => {
+        let param = ''
+        if (idx >= arr.length -2) {
+            param = 'hidden lg:block'
+        }
+        sectionStarWars.innerHTML += 
+        (`<li class="mb-4 mx-2 lg:mx-1.5 ${param}">
             <img class="w-full" src="${produto.image}" alt="foto-produto">
             <div class="flex flex-col justify-items-start mt-2">
             <span class="text-base">${produto.nome}</span>
             <span class="text-base font-bold">${produto.preco}</span>
             <a href="detalhe_produto/${produto.id}"><button class="text-sm font-bold text-left text-blue-500">Ver produto</button></a>
             </div>
-        </li>`))
+        </li>`)
+    })
 
     produtosConsole.forEach(produto => sectionConsoles.innerHTML += 
         (`<li class="mb-4 mx-2 lg:mx-1.5">
